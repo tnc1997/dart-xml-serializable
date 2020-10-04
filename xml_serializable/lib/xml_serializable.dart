f0a62ed929ab256666ee61ed41a91eaa7748b3d3
Thomas Clark <tnc1997@virginmedia.com>
2020-10-04 10:00:48 +0100

    feat: add xml serializable (#2)

---
+++ xml_serializable/lib/xml_serializable.dart
@@ -0,0 +1,13 @@
+library xml_serializable;
+
+export 'package:xml_serializable/src/extensions/class_element_extensions.dart';
+export 'package:xml_serializable/src/extensions/constant_reader_extensions.dart';
+export 'package:xml_serializable/src/extensions/dart_object_extensions.dart';
+export 'package:xml_serializable/src/extensions/dart_type_extensions.dart';
+export 'package:xml_serializable/src/extensions/field_element_extensions.dart';
+export 'package:xml_serializable/src/generators/xml_serializable_generator.dart';
+export 'package:xml_serializable/src/type_checkers/xml_attribute_type_checker.dart';
+export 'package:xml_serializable/src/type_checkers/xml_element_type_checker.dart';
+export 'package:xml_serializable/src/type_checkers/xml_root_element_type_checker.dart';
+export 'package:xml_serializable/src/type_checkers/xml_serializable_type_checker.dart';
+export 'package:xml_serializable/src/type_checkers/xml_text_type_checker.dart';
