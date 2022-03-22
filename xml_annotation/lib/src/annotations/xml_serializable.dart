21b8f950c8f5650f845c43eb6e94215a7828fe6b
Thomas Clark <tnc1997@virginmedia.com>
2022-03-22 19:50:10 +0000

    wip: add create mixin to xml serializable annotation

--- xml_annotation/lib/src/annotations/xml_serializable.dart
+++ xml_annotation/lib/src/annotations/xml_serializable.dart
@@ -1,4 +1,25 @@
 /// An annotation used to specify that a class is serializable.
 class XmlSerializable {
-  const XmlSerializable();
+  /// If `true` (not the default) then a mixin will be created that contains the serialization methods.
+  ///
+  /// ```dart
+  /// @XmlSerializable({
+  ///   createMixin: true,
+  /// })
+  /// class Example with _$ExampleXmlSerializer {
+  ///   @XmlText()
+  ///   String? text;
+  ///
+  ///   Example({
+  ///     this.text,
+  ///   });
+  ///
+  ///   factory Example.fromXmlElement(XmlElement element) => _$ExampleFromXmlElement(element);
+  /// }
+  /// ```
+  final bool? createMixin;
+
+  const XmlSerializable({
+    this.createMixin,
+  });
 }
