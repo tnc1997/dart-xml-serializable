bcbd939b7509d498b4198168d466c324ce5525ba
Thomas Clark <tnc1997@virginmedia.com>
2022-10-01 14:39:37 +0100

    docs: add xml value annotation

--- xml_annotation/lib/src/annotations/xml_value.dart
+++ xml_annotation/lib/src/annotations/xml_value.dart
@@ -1,4 +1,19 @@
-/// An annotation used to specify how a enum value is serialized.
+/// An annotation used to specify how an enum value is serialized.
+///
+/// ```dart
+/// enum Language {
+///   @XmlValue('Mandarin')
+///   mandarin,
+///   @XmlValue('Spanish')
+///   spanish,
+///   @XmlValue('English')
+///   english,
+///   @XmlValue('Hindi')
+///   hindi,
+///   @XmlValue('Bengali')
+///   bengali,
+/// }
+/// ```
 class XmlValue {
   /// The value to use when serializing and deserializing.
   final String value;
