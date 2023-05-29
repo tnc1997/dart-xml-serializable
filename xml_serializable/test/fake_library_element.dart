ca0e835cb95fdc229c0303c09b79d5ea759897bc
Thomas Clark <tnc1997@virginmedia.com>
2023-05-29 21:00:34 +0100

    feat: add support for import prefixes (#40)

--- xml_serializable/test/fake_library_element.dart
+++ xml_serializable/test/fake_library_element.dart
@@ -12,15 +12,25 @@ class FakeLibraryElement extends Fake implements LibraryElement {
   final bool isNonNullableByDefault;
 
   @override
+  final List<LibraryImportElement> libraryImports;
+
+  @override
   final String name;
+
+  @override
+  final Iterable<Element> topLevelElements;
 
   FakeLibraryElement({
     String? identifier,
     bool? isDartCore,
     bool? isNonNullableByDefault,
+    List<LibraryImportElement>? libraryImports,
     String? name,
+    Iterable<Element>? topLevelElements,
   })  : identifier = identifier ?? '',
         isDartCore = isDartCore ?? false,
+        libraryImports = libraryImports ?? [],
         isNonNullableByDefault = isNonNullableByDefault ?? true,
-        name = name ?? '';
+        name = name ?? '',
+        topLevelElements = topLevelElements ?? [];
 }
